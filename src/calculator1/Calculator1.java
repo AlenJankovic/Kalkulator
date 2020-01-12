4a837f662fa8973f312d0953af644cbb5adc8fab
alenj <alen.jankovic@bredband.net>
2020-01-13 00:49:14 +0100

    no operand

--- src/calculator1/Calculator1.java
+++ src/calculator1/Calculator1.java
@@ -147,6 +147,55 @@ class Calculator1 extends JFrame implements ActionListener{
                         // set the value of text 
 			l.setText(s0 + s1 + s2); 
                 }
+                else if (s.charAt(0) == '=') { 
+
+			double te; 
+
+			// store the value in 1st 
+			if (s1.equals("+")) 
+                                te = (Double.parseDouble(s0) + Double.parseDouble(s2)); 
+			else if (s1.equals("-")) 
+				te = (Double.parseDouble(s0) - Double.parseDouble(s2)); 
+			else if (s1.equals("/")) 
+				te = (Double.parseDouble(s0) / Double.parseDouble(s2)); 
+			else
+				te = (Double.parseDouble(s0) * Double.parseDouble(s2)); 
+                        // set the value of text 
+			l.setText(s0 + s1 + s2 + "=" + te); 
+
+			// convert it to string 
+			s0 = Double.toString(te); 
+
+			s1 = s2 = ""; 
+		} 
+                else { 
+			// if there was no operand 
+			if (s1.equals("") || s2.equals("")) 
+				s1 = s; 
+			// else evaluate 
+			else { 
+				double te; 
+                                
+                       // store the value in 1st 
+			if (s1.equals("+")) 
+				te = (Double.parseDouble(s0) + Double.parseDouble(s2)); 
+                        else if (s1.equals("-")) 
+				te = (Double.parseDouble(s0) - Double.parseDouble(s2)); 
+                        else if (s1.equals("/")) 
+				te = (Double.parseDouble(s0) / Double.parseDouble(s2)); 
+                        else
+				te = (Double.parseDouble(s0) * Double.parseDouble(s2)); 
+                        
+                        // convert it to string 
+				s0 = Double.toString(te); 
+                        // place the operator 
+				s1 = s; 
+                        // make the operand blank 
+				s2 = ""; 
+                }
+                        // set the value of text 
+			l.setText(s0 + s1 + s2); 
+                }
     }
 
 }
