d264c1773c4608002eeed6135d1a66164277f5ae
alenj <alen.jankovic@bredband.net>
2020-01-12 21:22:01 +0100

    create frame

--- src/calculator1/Calculator1.java
+++ src/calculator1/Calculator1.java
@@ -1,17 +1,48 @@
 /*
  * 
- * 
+ * Simple calculator
  * 
  */
 
 package calculator1;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
-public class Calculator1 {
-
+class Calculator1 extends JFrame implements ActionListener{
+    //create a frame 
+    static JFrame f;
+    //create text field
+    static JTextField l; 
+    //operator and operands
+    String s0, s1, s2;
    
+    //def. constructor
+    Calculator1() 
+	{ 
+		s0 = s1 = s2 = ""; 
+	} 
+    //main 
     public static void main(String[] args) {
       
+        // create a frame 
+		f = new JFrame("calculator"); 
+
+        try { 
+			// set look and feel 
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
+		} 
+		catch (Exception e) { 
+			System.err.println(e.getMessage()); 
+		} 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
